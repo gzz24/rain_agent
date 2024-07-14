177b7afb50f4cffe5cc8203a48d57e6106f4cc0d
1170500820 <21s003017@stu.hit.edu.cn>
2024-07-14 14:54:47 +0800

    <feat>对话引入历史

--- rain_agent/app.py
+++ rain_agent/app.py
@@ -1,10 +1,14 @@
 import os
 import glob
+import uuid
 from configs import config
 
 import streamlit as st
 from openai import OpenAI
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+from langchain_core.chat_history import BaseChatMessageHistory, InMemoryChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
 
 
 # 暂时只有这些应用
@@ -20,24 +24,46 @@ with st.sidebar:
     selected_agent = st.selectbox('我的Agent', op_lst, placeholder=op_lst[0])
 
 if selected_agent == 'naive_chat':
+    def get_session_history(session_id: str) -> BaseChatMessageHistory:
+        if session_id not in st.session_state.store:
+            st.session_state.store[session_id] = InMemoryChatMessageHistory()
+        return st.session_state.store[session_id]
+
+    def stream_response(res, store):
+        for chunk in res:
+            store.append(chunk.content)
+            yield chunk.content
+
+    # history for streamlit
     if 'messages' not in st.session_state:
         st.session_state.messages = []
+        conversation_uuid = str(uuid.uuid4())
+        st.session_state.uuid = conversation_uuid
+        st.session_state.store = {}
+    # re-render history conversation
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
+    # clear button
     with st.sidebar:
         clear_his = st.button('清除对话历史')
         if clear_his:
             st.session_state.messages = []
+            st.session_state.uuid = str(uuid.uuid4())
+            st.session_state.store = {}
             st.rerun()  # tricky!
 
+    # chat instance
     llm = ChatOpenAI(
         model='qwen-max',
         openai_api_key=config['key']['bailian_api_key'],
         openai_api_base=config['key']['bailian_base_url']
     )
+    with_message_history = RunnableWithMessageHistory(llm, get_session_history)
+    config = {'configurable': {'session_id': st.session_state.uuid}}
 
+    # get input
     if prompt := st.chat_input('input your prompt'):
         with st.chat_message('user'):
             st.markdown(prompt)
@@ -46,12 +72,10 @@ if selected_agent == 'naive_chat':
             'content': prompt
         })
 
-        response = llm.stream(prompt)
-        def stream_response(res, store):
-            for chunk in res:
-                store.append(chunk.content)
-                yield chunk.content
-
+        response = with_message_history.stream(
+            [HumanMessage(content=prompt)],
+            config=config
+        )
         with st.chat_message('assistant'):
             temp_store = []
             st.write_stream(stream_response(response, temp_store))
