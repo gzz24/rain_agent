dc81f583f58717f6ac2fcee27ae755c3ea43fe3c
1170500820 <21s003017@stu.hit.edu.cn>
2024-07-23 07:43:04 +0800

    <feat>a little updates to funcitn agent

--- rain_agent/agent_apps/paper_function/pf_agent.py
+++ rain_agent/agent_apps/paper_function/pf_agent.py
@@ -10,6 +10,18 @@ from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langgraph.graph import add_messages, START, END, StateGraph
 
+from rain_agent.configs import config
+
 
 class FunctionState(TypedDict):
     functions: List[str]
+
+
+
+# Models
+llm_core = ChatOpenAI(
+    model='qwen-max',
+    openai_api_key=config['key']['bailian_api_key'],
+    openai_api_base=config['key']['bailian_base_url']
+)
+
